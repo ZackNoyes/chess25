71b4aa0b581f991dc11e414e23714d9ee880590d
Zack Noyes <u6867384@anu.edu.au>
2023-07-17 18:36:28 +1000

    set up files and webpack config for another page

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,14 +1,30 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
-module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+module.exports = [
+  {
+    entry: "./bootstrap.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bootstrap.js",
+    },
+    mode: "development",
+    plugins: [
+      new CopyWebpackPlugin(['public', 'index.html'])
+    ],
   },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['public', 'index.html'])
-  ],
-};
+  {
+    entry: "./bootstrap-share.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "share/share.js",
+    },
+    mode: "development",
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: 'share.html',
+        to: 'share/index.html',
+      }])
+    ],
+  },
+];
