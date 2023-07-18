c418a1170f23b6b731c37bbec93c0cfd6d12ffca
Zack Noyes <u6867384@anu.edu.au>
2023-07-18 13:15:13 +1000

    completed sharing and comparing functionality

--- www/share.js
+++ www/share.js
@@ -1,6 +1,9 @@
 import { drawHistory } from "./utils";
 import { CANVAS_SIZE } from "./constants";
 
+var games = [];
+var currentMove = 1;
+
 let b64Game = new URLSearchParams(window.location.search).get('game');
 let gameString = atob(b64Game);
 let split = gameString.split(':');
@@ -86,9 +89,9 @@ if (request.status == 200) {
   console.error("Unexpected status code: " + request.status);
 }
 
-// TODO: border coloring
-
 if (showGames) {
+
+  document.getElementById('gamesDisplay').style.display = 'block';
 
   if (myGame != null && theirGame != null) {
     
@@ -96,8 +99,8 @@ if (showGames) {
     document.getElementById('ownResult').innerHTML = myGame.resultString;
     document.getElementById('theirResult').innerHTML = theirGame.resultString;
 
-    fillCol(document.getElementById('ownGameCol'), myGame);
-    fillCol(document.getElementById('theirGameCol'), theirGame);
+    addCanvas(document.getElementById('ownGameCol'), myGame);
+    addCanvas(document.getElementById('theirGameCol'), theirGame);
 
   } else if (myGame != null) {
 
@@ -105,7 +108,7 @@ if (showGames) {
     document.getElementById('ownGameExplanation').style.display = 'block';
     document.getElementById('singleResult').innerHTML = myGame.resultString;
 
-    fillCol(document.getElementById('singleGameCol'), myGame);
+    addCanvas(document.getElementById('singleGameCol'), myGame);
 
   } else if (theirGame != null) {
 
@@ -113,20 +116,113 @@ if (showGames) {
     document.getElementById('onlyTheirGameExplanation').style.display = 'block';
     document.getElementById('singleResult').innerHTML = theirGame.resultString;
 
-    fillCol(document.getElementById('singleGameCol'), theirGame);
+    addCanvas(document.getElementById('singleGameCol'), theirGame);
 
+  }
+
+  if (myGame != null) {
+    document.getElementById('shareButton').style.display = 'inline';
   }
 
 }
 
-function fillCol(colElem, game) {
-  for (let i = 0; i < game.turns.length; i++) {
-    let canvas = document.createElement('canvas');
-    canvas.classList.add('static-canvas');
-    canvas.width = CANVAS_SIZE;
-    canvas.height = CANVAS_SIZE;
-    colElem.appendChild(canvas);
-    var context = canvas.getContext('2d');
-    drawHistory(context, i, game.history);
+function addCanvas(colElem, game) {
+  let canvas = document.createElement('canvas');
+  canvas.classList.add('static-canvas');
+  canvas.width = CANVAS_SIZE;
+  canvas.height = CANVAS_SIZE;
+  colElem.appendChild(canvas);
+  var context = canvas.getContext('2d');
+
+  let turnLabel = document.createElement('p');
+  colElem.appendChild(turnLabel);
+
+  games.push({context: context, game: game, turnLabel: turnLabel});
+  draw();
+}
+
+function incMove() {
+  currentMove++;
+  var overshot = true;
+  for (let game of games) {
+    if (game.game.turns.length >= currentMove) {
+      overshot = false;
+      break;
+    }
+  }
+  if (overshot) { currentMove--; }
+  draw();
+}
+window.incMove = incMove;
+
+function decMove() {
+  currentMove--;
+  if (currentMove < 1) { currentMove = 1; }
+  draw();
+}
+window.decMove = decMove;
+
+function draw() {
+
+  var longestTurns = []
+
+  for (let game of games) {
+    let move = Math.min(currentMove, game.game.turns.length);
+    drawHistory(game.context, move - 1, game.game.history);
+    game.turnLabel.innerHTML = "move " + move;
+    if (game.game.turns.length > longestTurns.length) {
+      longestTurns = game.game.turns;
+    }
+  }
+
+  document.getElementById("moveHistory").innerHTML = "";
+  for (let i = 0; i < longestTurns.length; i++) {
+    let box = document.createElement('button');
+    box.classList.add(longestTurns[i] == "w" ? "white-box" : "black-box");
+    if (i + 1 == currentMove) {
+      box.classList.add("current-move");
+    }
+    for (let game of games) {
+      if (game.game.turns.length == i + 1) {
+        box.classList.add("final-move");
+      }
+    }
+    box.onclick = function() {
+      currentMove = i + 1;
+      draw();
+    };
+    document.getElementById("moveHistory").appendChild(box);
+  }
+
+  document.getElementById("decButton").disabled = currentMove == 1;
+  document.getElementById("incButton").disabled = currentMove == longestTurns.length;
+
+}
+
+document.onkeydown = function(e) {
+  if (e.key == "ArrowLeft") {
+    decMove();
+  }
+  if (e.key == "ArrowRight") {
+    incMove();
   }
 }
+
+function shareGame() {
+  let button = document.getElementById("shareButton");
+  navigator.clipboard.writeText(window.location.hostname + "/share/?game=" + btoa(localStorage.getItem("userID") + ":" + dateString)).then(() => {
+    var tooltip = new bootstrap.Tooltip(button, {title: "Link copied to clipboard"});
+    tooltip.show();
+    setTimeout(() => {
+      tooltip.hide();
+    }, 2000);
+  },
+  () => {
+    var tooltip = new bootstrap.Tooltip(button, {title: "Link copied to clipboard"});
+    tooltip.show();
+    setTimeout(() => {
+      tooltip.hide();
+    }, 2000);
+  });
+}
+window.shareGame = shareGame;
